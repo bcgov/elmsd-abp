005361a8476231741a4ab45740973d7afbc50637
npham49 <brian.d.pham@gov.bc.ca>
2023-09-29 10:55:50 -0700

    added applications to apps folder instead of packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     extends: ["@bcgov-elmsd/eslint-config"],
     parserOptions: { project: "tsconfig.json", tsconfigRootDir: __dirname, sourceType: "module" },
     rules: {
-        "prettier/prettier": ["error", prettierConfig]
+        "prettier/prettier": ["error", prettierConfig],
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-shadow": "off"
     },
     ignorePatterns: ["**/dist/**/*.js"]
 }
